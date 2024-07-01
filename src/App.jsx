bc27da8e184b8feef67b90ed399d77ed61ecfcdd
smwrhasan <107409331+smwrhasan@users.noreply.github.com>
2024-07-01 00:57:40 +0000

    name changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "S.M. Waliur Rahman Hasan",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  email: "s.m.w.r.hasan@gmail.com",
+  gitHub: "smwrhasan",
+  instagram: "",
+  linkedIn: "",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "smwrhasan",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
